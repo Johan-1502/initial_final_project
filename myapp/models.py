29938228677780c93515c0c38ef4129ad89899e8
Edison <edison.gutierrez01@uptc.edu.co>
2025-05-05 11:44:59 -0500

    fix: added primary key and not blank fields

--- myapp/models.py
+++ myapp/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    name = models.CharField(max_length=200)
-    dni = models.CharField(max_length=20)
-    phoneNumber = models.CharField(max_length=15)
-    address = models.CharField(max_length=100)
-    email = models.EmailField()
+    name = models.CharField(max_length=200, blank=False)
+    dni = models.CharField(max_length=20, primary_key=True, blank=False)
+    phoneNumber = models.CharField(max_length=15, blank=False)
+    address = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(blank=False)
     isVisible = models.BooleanField(default=True)
+
     def __str__(self):
         return self.name + ' ' + self.dni
     
